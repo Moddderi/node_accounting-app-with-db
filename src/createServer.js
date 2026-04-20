80ddb32bb2df86e3e307f80ff2cb657ced9c62da
Oleh Kirich <olehkirich@MacBook--Oleh.local>
2026-04-20 14:16:45 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const express = require('express');
+const cors = require('cors');
+const usersRouter = require('./api/users.router');
+const expensesRouter = require('./api/expenses.router');
+const categoriesRouter = require('./api/categories.router');
+
 const createServer = () => {
-  // your code goes here
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use('/users', express.json(), usersRouter);
+  app.use('/expenses', express.json(), expensesRouter);
+  app.use('/categories', express.json(), categoriesRouter);
+
+  return app;
 };
 
 module.exports = {
